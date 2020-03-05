331a77050bd750c1c9750f2db1945c4083181bb6
banana <jgjin@umich.edu>
2020-03-05 02:54:34 -0500

    remove refactored code

--- scrape.rb
+++ scrape.rb
@@ -33,31 +33,6 @@ rescue StandardError => e
   raise e
 end
 
-def get_term_code_cookies(term_code)
-  uri = URI.parse("#{HOSTNAME}/StudentRegistrationSsb/ssb/term/search?mode=courseSearch")
-  request = Net::HTTP::Post.new(uri)
-  request.form_data = { 'dataType' => 'json', 'term' => term_code['code'] }
-  response = request_or_retry(request, uri)
-  cookies = response.response['set-cookie']
-  cookies
-end
-
-# @return [Array<Hash>]
-def term_codes
-  uri = URI.parse("#{HOSTNAME}/StudentRegistrationSsb/ssb/courseSearch/getTerms?dataType=json&offset=1&max=1000")
-  request = Net::HTTP::Get.new(uri)
-  response = request_or_retry(request, uri)
-  JSON.parse(response.body)
-end
-
-# @param [Hash] term_code
-def depts_for_term_code(term_code)
-  uri = URI.parse("#{HOSTNAME}/StudentRegistrationSsb/ssb/classSearch/get_subject?searchTerm=&term=#{term_code['code']}&offset=1&max=10000")
-  request = Net::HTTP::Get.new(uri)
-  response = request_or_retry(request, uri)
-  JSON.parse(response.body)
-end
-
 def retrieve_professor(banner_id, term_code, cookies)
   uri = URI.parse("#{HOSTNAME}/StudentRegistrationSsb/ssb/contactCard/retrieveData?bannerId=#{banner_id}&termCode=#{term_code['code']}")
   request = Net::HTTP::Get.new(uri)
@@ -98,37 +73,3 @@ def list_sections_for_dept(dept, term_code, cookies)
   end
   results
 end
-
-def main
-  term_codes.each do |term_code|
-    puts term_code['code']
-    departments = depts_for_term_code(term_code)
-    FileUtils.mkdir_p("data/#{term_code['code']}")
-    departments.each do |dept|
-      cookies = get_term_code_cookies(term_code)
-      puts dept['code']
-      sections = list_sections_for_dept(dept, term_code, cookies)
-      sections.collect do |section|
-        section if section['faculty'].empty?
-        section['faculty'] = section['faculty'].collect do |faculty|
-          display_name = faculty['displayName']
-          unless DISPLAY_NAME_CACHE.key? display_name
-            detailed_professor = retrieve_professor(faculty['bannerId'], term_code, cookies)
-            next if detailed_professor.nil?
-          end
-          DISPLAY_NAME_CACHE[display_name]
-        end
-        section['faculty'].compact!
-        section
-      end
-      File.open("data/#{term_code['code']}/#{dept['code']}.json", 'w+') do |f|
-        f.write(sections.to_json)
-      end
-      File.open(CACHE_FILE_LOCATION, 'w+') do |f|
-        f.write(DISPLAY_NAME_CACHE.to_json)
-      end
-    end
-  end
-end
-
-main

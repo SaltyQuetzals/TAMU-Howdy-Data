809751b428efd31c92530f0787fdcee6c20f4330
banana <jgjin@umich.edu>
2020-03-05 17:20:44 -0500

    main.rb with async and cache

--- main.rb
+++ main.rb
@@ -2,68 +2,70 @@ require 'async'
 require 'fileutils'
 require 'json'
 
+require_relative 'cache.rb'
+require_relative 'term.rb'
 require_relative 'utils.rb'
-require_relative 'scrape.rb'
+
+CACHE = Cache.new('data/cache.json')
 
 def terms
-  response = request('/StudentRegistrationSsb/ssb/courseSearch/getTerms?dataType=json&offset=1&max=1728')
+  response = request('/StudentRegistrationSsb/ssb/courseSearch/getTerms?dataType=json&offset=1&max=2')
   JSON.parse(response.body)
 end
 
-def async_process_term(term_code)
+def async_process_term(term)
   Async do |_|
-    FileUtils.mkdir_p("data/%s" % term_code)
-    cookies = get_term_code_cookies term_code
-    depts_for_term(term_code).each do |dept|
-      async_process_dept(term_code, dept, cookies)
+    term_code = term['code']
+    FileUtils.mkdir_p("data/#{term_code}")
+    term = Term.new(term_code)
+    term.departments.each do |dept|
+      async_process_dept(term, dept)
     end
   end
 end
 
-def get_term_code_cookies(term_code)
-  form_data = { 'dataType' => 'json', 'term' => term_code }.to_json
-  response = request('/StudentRegistrationSsb/ssb/term/search?mode=courseSearch', HttpMethod::POST, form_data)
-  response.headers['set-cookie']
+def async_process_dept(term, dept)
+  puts "processing term=#{term.term_code}, dept=#{dept['code']}"
+  Async do |_|
+    sections_async = term.sections(dept).collect do |section|
+      async_process_section(term, section)
+    end
+    sections = sections_async.map { |section| section.wait }
+    File.open("data/#{term.term_code}/#{dept['code']}.json", 'w') do |output|
+      output.write(sections.to_json)
+    end
+    CACHE.update_to_disk
+  end
 end
 
-def depts_for_term(term_code)
-  response = request('/StudentRegistrationSsb/ssb/classSearch/get_subject?searchTerm=&term=%s&offset=1&max=1728' % term_code)
-  JSON.parse(response.body)
-end
-
-def async_process_dept(term_code, dept, cookies)
+def async_process_section(term, section)
   Async do |task|
-    # back to Hash for compatibility
-    term_code = {'code' => term_code}
-    puts "processing term %s, dept %s" % [term_code, dept['code']]
-    # old code
-    sections = list_sections_for_dept(dept, term_code, cookies)
-    sections.collect do |section|
-      section if section['faculty'].empty?
-      section['faculty'] = section['faculty'].collect do |faculty|
-        display_name = faculty['displayName']
-        unless DISPLAY_NAME_CACHE.key? display_name
-          detailed_professor = retrieve_professor(faculty['bannerId'], term_code, cookies)
-          next if detailed_professor.nil?
-        end
-        DISPLAY_NAME_CACHE[display_name]
+    section if section['faculty'].empty?
+    section['faculty'] = section['faculty'].collect do |faculty|
+      async_process_faculty(term, faculty)
+    end
+    section['faculty'].compact!
+    section
+  end
+end
+
+def async_process_faculty(term, faculty)
+  Async do |_|
+    display_name = faculty['displayName']
+    unless CACHE.contains(display_name)
+      person_data = term.get_faculty(faculty)
+      if person_data
+        CACHE.insert(display_name, person_data)
       end
-      section['faculty'].compact!
-      section
     end
-    File.open("data/#{term_code['code']}/#{dept['code']}.json", 'w+') do |f|
-      f.write(sections.to_json)
-    end
-    File.open(CACHE_FILE_LOCATION, 'w+') do |f|
-      f.write(DISPLAY_NAME_CACHE.to_json)
-    end
+    CACHE.read(display_name)
   end
 end
 
 def main
   Async do |_|
     terms.each do |term|
-      async_process_term term['code']
+      async_process_term term
     end
   end
 end

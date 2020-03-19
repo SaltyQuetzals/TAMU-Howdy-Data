cfe60c7fa1de78ae55878fe7d70848827fcd1da2
Gabriel Britain <quontas@gmail.com>
2020-03-19 13:00:08 -0500

    Parallelized better.

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,75 @@
-import {Term} from './term';
+import { Term } from './term';
 import * as fs from 'fs';
-import {CompassDepartment} from "./compass";
+import { CompassDepartment } from './compass';
+import { promisify } from 'util';
 
-const downloadDepartmentData = async (term: Term, dept: CompassDepartment, retriesRemaining = Term.MAX_RETRIES): Promise<CompassDepartment> => {
-    while (retriesRemaining > 0) {
-        try {
-            dept.courses = {};
-            const courses = await term.courses(dept.code);
+const promisifiedWriteFile = promisify(fs.writeFile);
 
-            for (const course of courses) {
-                course.sections = [];
-                dept.courses[course.courseNumber] = course;
-            }
+const downloadDepartmentData = async (
+  term: Term,
+  dept: CompassDepartment,
+  retriesRemaining = Term.MAX_RETRIES
+): Promise<CompassDepartment> => {
+  while (retriesRemaining > 0) {
+    try {
+      dept.courses = {};
+      const courses = await term.courses(dept.code);
 
-            const sections = await term.sectionsForDepartment(dept.code, true);
-            for (const section of sections) {
-                dept.courses[section.courseNumber].sections!.push(section);
-            }
-            return dept;
-        } catch (err) {
-            console.log(`Failed to download ${dept.code}.`);
-            console.error(err);
-            retriesRemaining -= 1;
-        }
+      for (const course of courses) {
+        course.sections = [];
+        dept.courses[course.courseNumber] = course;
+      }
+
+      const sections = await term.sectionsForDepartment(dept.code, true);
+      for (const section of sections) {
+        dept.courses[section.courseNumber].sections!.push(section);
+      }
+      return dept;
+    } catch (err) {
+      console.log(`Failed to download ${dept.code}.`);
+      console.error(err);
+      retriesRemaining -= 1;
     }
-    throw new Error(`Failed to download ${dept.code} after ${Term.MAX_RETRIES} tries.`);
-}
+  }
+  throw new Error(
+    `Failed to download ${dept.code} after ${Term.MAX_RETRIES} tries.`
+  );
+};
 
 const collectAllData = async (term: Term) => {
-    const departments = await term.departments();
-    //
-    // const departmentPromises = [];
-    // for (const department of departments) {
-    //     departmentPromises.push(downloadDepartmentData(term, department).then((departmentData) => {
-    //         fs.writeFile(
-    //                 `data/${term.termCode}/${departmentData.code}.json`,
-    //                 JSON.stringify(departmentData, null, 3),
-    //                 err => {
-    //                     if (err) {
-    //                         console.error(err);
-    //                     }
-    //                     console.log(departmentData.code);
-    //                 }
-    //             );
-    //     }))
-    // }
-    // await Promise.all(departmentPromises);
-    for (const department of departments) {
-        const departmentData = await downloadDepartmentData(term, department);
-        fs.writeFile(
+  const departments = await term.departments();
+  const departmentPromises = [];
+  for (const department of departments) {
+    departmentPromises.push(
+      downloadDepartmentData(new Term(term.termCode), department).then(
+        departmentData =>
+          promisifiedWriteFile(
             `data/${term.termCode}/${departmentData.code}.json`,
-            JSON.stringify(departmentData, null, 3),
-            err => {
-                if (err) {
-                    console.error(err);
-                }
-                console.log(departmentData.code);
-            }
-        );
-    }
+            JSON.stringify(departmentData, null, 3)
+          ).then(() => {
+            console.log(department.code);
+          })
+      )
+    );
+  }
+  await Promise.all(departmentPromises);
 };
 
 async function main() {
-    if (!fs.existsSync('data')) {
-        fs.mkdirSync('data');
+  if (!fs.existsSync('data')) {
+    fs.mkdirSync('data');
+  }
+  const terms = await Term.allTerms();
+  for (const { code } of terms) {
+    console.log('====================================================');
+    console.log(code);
+    console.log('====================================================');
+    if (!fs.existsSync(`data/${code}`)) {
+      fs.mkdirSync(`data/${code}`);
     }
-    const terms = await Term.allTerms();
-    for (const {code} of terms) {
-        console.log('====================================================');
-        console.log(code);
-        console.log('====================================================');
-        if (!fs.existsSync(`data/${code}`)) {
-            fs.mkdirSync(`data/${code}`);
-        }
-        const term = new Term(code);
-        await collectAllData(term);
-    }
+    const term = new Term(code);
+    await collectAllData(term);
+  }
 }
 
 main();
--- src/term.ts
+++ src/term.ts
@@ -1,238 +1,244 @@
-import Axios, {AxiosInstance, AxiosResponse} from 'axios';
+import Axios, { AxiosInstance, AxiosResponse } from 'axios';
 import * as qs from 'querystring';
 import {
-    CompassCourse,
-    CompassDepartment,
-    CompassFaculty,
-    CompassSection,
-    CompassSectionFaculty,
-    PaginatedCompassResponse,
-    TermCode,
+  CompassCourse,
+  CompassDepartment,
+  CompassFaculty,
+  CompassSection,
+  CompassSectionFaculty,
+  PaginatedCompassResponse,
+  TermCode,
 } from './compass';
-import {URL} from 'url';
+import { URL } from 'url';
 import axiosRetry from 'axios-retry';
 import axiosCookieJarSupport from 'axios-cookiejar-support';
 import * as tough from 'tough-cookie';
-import {setupCache} from 'axios-cache-adapter';
+import { setupCache } from 'axios-cache-adapter';
 
 // Create `axios-cache-adapter` instance
 const CACHE = setupCache({
-    maxAge: 24 * 60 * 1000,
+  maxAge: 24 * 60 * 1000,
 });
+
+const PROF_CACHE = new Map<string, CompassFaculty>();
 
 const BASE_URL = 'https://compassxe-ssb.tamu.edu';
 
 export class Term {
-    static MAX_PAGE_SIZE = 500;
-    static MAX_RETRIES = 10;
-    private readonly client: AxiosInstance;
-    private jar: tough.CookieJar;
-    private professorCache: Map<string, CompassFaculty>;
+  static MAX_PAGE_SIZE = 500;
+  static MAX_RETRIES = 10;
+  private readonly client: AxiosInstance;
+  private jar: tough.CookieJar;
 
-    constructor(public termCode: TermCode) {
-        this.client = Axios.create({
-            baseURL: BASE_URL,
-            jar: new tough.CookieJar(),
-            adapter: CACHE.adapter,
-        });
-        this.professorCache = new Map<string, CompassFaculty>();
-        axiosCookieJarSupport(this.client);
-        this.jar = new tough.CookieJar();
-        this.client.defaults.jar = this.jar;
-        axiosRetry(this.client, {retries: Term.MAX_RETRIES});
+  constructor(public termCode: TermCode) {
+    this.client = Axios.create({
+      baseURL: BASE_URL,
+      jar: new tough.CookieJar(),
+      adapter: CACHE.adapter,
+    });
+    axiosCookieJarSupport(this.client);
+    this.jar = new tough.CookieJar();
+    this.client.defaults.jar = this.jar;
+    axiosRetry(this.client, { retries: Term.MAX_RETRIES });
+  }
+
+  /**
+   * Spoofs getting cookies from Compass, which are needed for the API to deliver consistent results.
+   * Without cookies, some responses will contain incorrect data despite URLs being correct.
+   */
+  private async addCookies() {
+    const endpoint =
+      '/StudentRegistrationSsb/ssb/term/search?mode=courseSearch';
+    const formData = {
+      dataType: 'json',
+      term: this.termCode,
+    };
+    await this.client.post(endpoint, qs.stringify(formData));
+  }
+
+  /**
+   * Will request all paginated data at a certain endpoint.
+   * @param pathString The endpoint to hit
+   * @param fetchOne The function for retrieving a single page of data (will be run for each page)
+   */
+  private async fetchAndCombinePaginatedResponses<T>(
+    pathString: string,
+    fetchOne: (urlString: string) => Promise<T[]>
+  ): Promise<T[]> {
+    const url = new URL(BASE_URL + pathString);
+    url.searchParams.set('pageMaxSize', String(Term.MAX_PAGE_SIZE));
+    const collected: T[] = [];
+    while (true) {
+      url.searchParams.set('pageOffset', String(collected.length));
+      const newData = await fetchOne(url.toString());
+      if (!newData || newData.length === 0) break;
+      collected.push(...newData);
     }
+    return collected;
+  }
 
-    /**
-     * Spoofs getting cookies from Compass, which are needed for the API to deliver consistent results.
-     * Without cookies, some responses will contain incorrect data despite URLs being correct.
-     */
-    private async addCookies() {
-        const endpoint =
-            '/StudentRegistrationSsb/ssb/term/search?mode=courseSearch';
-        const formData = {
-            dataType: 'json',
-            term: this.termCode,
-        };
-        await this.client.post(endpoint, qs.stringify(formData));
+  /**
+   * Retrieves all departments offered during this term.
+   */
+  async departments(): Promise<CompassDepartment[]> {
+    const endpoint = `/StudentRegistrationSsb/ssb/classSearch/get_subject?searchTerm=&term=${this.termCode}&offset=1&max=1728`;
+    const response: AxiosResponse<CompassDepartment[]> = await this.client.get(
+      endpoint,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  }
+
+  /**
+   * Gets the full HTML course description of a Compass course
+   */
+  private async getCourseDescription(
+    subject: string,
+    courseNum: string
+  ): Promise<string> {
+    const endpoint = `/StudentRegistrationSsb/ssb/courseSearchResults/getCourseDescription`;
+    const payload = {
+      term: this.termCode,
+      subjectCode: subject,
+      courseNumber: courseNum,
+    };
+    const response = await this.client.post(endpoint, qs.stringify(payload));
+    return response.data.trim();
+  }
+
+  /**
+   * Helper function for updating a set of courses with their full descriptions
+   * @param courses A list of Compass courses.
+   */
+  private async updateEachCourseWithFullDescription(courses: CompassCourse[]) {
+    for (let i = 0; i < courses.length; ++i) {
+      courses[i].courseDescription = await this.getCourseDescription(
+        courses[i].subjectCode,
+        courses[i].courseNumber
+      );
     }
+    return courses;
+  }
 
-    /**
-     * Will request all paginated data at a certain endpoint.
-     * @param pathString The endpoint to hit
-     * @param fetchOne The function for retrieving a single page of data (will be run for each page)
-     */
-    private async fetchAndCombinePaginatedResponses<T>(
-        pathString: string,
-        fetchOne: (urlString: string) => Promise<T[]>
-    ): Promise<T[]> {
-        const url = new URL(BASE_URL + pathString);
-        url.searchParams.set('pageMaxSize', String(Term.MAX_PAGE_SIZE));
-        const collected: T[] = [];
-        while (true) {
-            url.searchParams.set('pageOffset', String(collected.length));
-            const newData = await fetchOne(url.toString());
-            if (!newData || newData.length === 0) break;
-            collected.push(...newData);
-        }
-        return collected;
+  /**
+   * Requests all courses for a specific department during this term.
+   * @param dept The given department code.
+   */
+  async courses(dept: string): Promise<CompassCourse[]> {
+    const endpoint = `/StudentRegistrationSsb/ssb/courseSearchResults/courseSearchResults?txt_subject=${dept}&txt_term=${this.termCode}`;
+    const compassCourses = await this.fetchAndCombinePaginatedResponses<
+      CompassCourse
+    >(endpoint, urlString =>
+      this.client
+        .get(urlString)
+        .then(
+          (response: AxiosResponse<PaginatedCompassResponse<CompassCourse>>) =>
+            response.data.data
+        )
+        .catch(err => {
+          console.error(`Error fetching ${urlString}`);
+          throw err;
+        })
+    );
+    return this.updateEachCourseWithFullDescription(compassCourses);
+  }
+
+  /**
+   * Replaces the sections in the given sectionPromise with fully-hydrated faculty members.
+   * @param sectionPromise
+   */
+  private async addFullFaculty(
+    sectionPromise: Promise<CompassSection[]>
+  ): Promise<CompassSection[]> {
+    return sectionPromise.then(sections => {
+      return Promise.all(
+        sections.map(section => {
+          return this.faculty(
+            ...(section.faculty as CompassSectionFaculty[])
+          ).then(fullFaculty => {
+            section.faculty = fullFaculty;
+            return section;
+          });
+        })
+      );
+    });
+  }
+
+  // async sectionsForCourse(subjectCourse: string, withFaculty = false) {
+  //   await this.addCookies();
+  //   const sectionPromise = this.fetchAndCombinePaginatedResponses<
+  //     CompassSection
+  //   >(
+  //     `/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subjectcoursecombo=${subjectCourse}&txt_term=${this.termCode}`,
+  //     urlString =>
+  //       this.client
+  //         .get(urlString, { withCredentials: true })
+  //         .then(response => response.data.data)
+  //   );
+  //   if (!withFaculty) {
+  //     return sectionPromise;
+  //   }
+  //   return this.addFullFaculty(sectionPromise);
+  // }
+
+  /**
+   * Gets all sections for a specific department in this term
+   * @param dept The code of the department in this term
+   * @param withFaculty Whether or not to add hydrated faculty
+   */
+  async sectionsForDepartment(dept: string, withFaculty = false) {
+    await this.addCookies();
+    const sectionPromise = this.fetchAndCombinePaginatedResponses<
+      CompassSection
+    >(
+      `/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subject=${dept}&txt_term=${this.termCode}`,
+      urlString =>
+        this.client
+          .get(urlString, { withCredentials: true })
+          .then(response => response.data.data)
+    );
+    if (!withFaculty) {
+      return sectionPromise;
     }
+    return this.addFullFaculty(sectionPromise);
+  }
 
-    /**
-     * Retrieves all departments offered during this term.
-     */
-    async departments(): Promise<CompassDepartment[]> {
-        const endpoint = `/StudentRegistrationSsb/ssb/classSearch/get_subject?searchTerm=&term=${this.termCode}&offset=1&max=1728`;
-        const response: AxiosResponse<CompassDepartment[]> = await this.client.get(
-            endpoint,
-            {
-                withCredentials: true,
-            }
+  /**
+   * Gets hydrated faculty members, given their "dehydrated" versions.
+   * @param facultyMembers
+   */
+  async faculty(
+    ...facultyMembers: CompassSectionFaculty[]
+  ): Promise<CompassFaculty[]> {
+    const fullFaculty: CompassFaculty[] = [];
+    for (const facultyMember of facultyMembers) {
+      if (!PROF_CACHE.has(facultyMember.displayName)) {
+        const response = await this.client.get(
+          `/StudentRegistrationSsb/ssb/contactCard/retrieveData?bannerId=${facultyMember.bannerId}&termCode=${this.termCode}`,
+          {
+            withCredentials: true,
+          }
         );
-        return response.data;
-    }
-
-    /**
-     * Gets the full HTML course description of a Compass course
-     */
-    private async getCourseDescription(
-        subject: string,
-        courseNum: string
-    ): Promise<string> {
-        const endpoint = `/StudentRegistrationSsb/ssb/courseSearchResults/getCourseDescription`;
-        const payload = {
-            term: this.termCode,
-            subjectCode: subject,
-            courseNumber: courseNum,
-        };
-        const response = await this.client.post(endpoint, qs.stringify(payload));
-        return response.data.trim();
-    }
-
-    /**
-     * Helper function for updating a set of courses with their full descriptions
-     * @param courses A list of Compass courses.
-     */
-    private async updateEachCourseWithFullDescription(courses: CompassCourse[]) {
-        for (let i = 0; i < courses.length; ++i) {
-            courses[i].courseDescription = await this.getCourseDescription(
-                courses[i].subjectCode,
-                courses[i].courseNumber
-            );
+        const fullMember = response.data.data.personData as CompassFaculty;
+        if (fullMember.cvExists) {
+          fullMember.cvUrl = BASE_URL + fullMember.cvUrl;
         }
-        return courses;
+        PROF_CACHE.set(facultyMember.displayName, fullMember);
+      }
+      fullFaculty.push(PROF_CACHE.get(facultyMember.displayName)!);
     }
+    return fullFaculty;
+  }
 
-    /**
-     * Requests all courses for a specific department during this term.
-     * @param dept The given department code.
-     */
-    async courses(dept: string): Promise<CompassCourse[]> {
-        const endpoint = `/StudentRegistrationSsb/ssb/courseSearchResults/courseSearchResults?txt_subject=${dept}&txt_term=${this.termCode}`;
-        const compassCourses = await this.fetchAndCombinePaginatedResponses<CompassCourse>(endpoint, urlString =>
-            this.client
-                .get(urlString)
-                .then(
-                    (response: AxiosResponse<PaginatedCompassResponse<CompassCourse>>) =>
-                        response.data.data
-                )
-                .catch(err => {
-                    console.error(`Error fetching ${urlString}`);
-                    throw err;
-                })
-        );
-        return this.updateEachCourseWithFullDescription(compassCourses);
-    }
-
-    /**
-     * Replaces the sections in the given sectionPromise with fully-hydrated faculty members.
-     * @param sectionPromise
-     */
-    private async addFullFaculty(
-        sectionPromise: Promise<CompassSection[]>
-    ): Promise<CompassSection[]> {
-        return sectionPromise.then(sections => {
-            return Promise.all(
-                sections.map(section => {
-                    return this.faculty(
-                        ...(section.faculty as CompassSectionFaculty[])
-                    ).then(fullFaculty => {
-                        section.faculty = fullFaculty;
-                        return section;
-                    });
-                })
-            );
-        });
-    }
-
-    // async sectionsForCourse(subjectCourse: string, withFaculty = false) {
-    //   await this.addCookies();
-    //   const sectionPromise = this.fetchAndCombinePaginatedResponses<
-    //     CompassSection
-    //   >(
-    //     `/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subjectcoursecombo=${subjectCourse}&txt_term=${this.termCode}`,
-    //     urlString =>
-    //       this.client
-    //         .get(urlString, { withCredentials: true })
-    //         .then(response => response.data.data)
-    //   );
-    //   if (!withFaculty) {
-    //     return sectionPromise;
-    //   }
-    //   return this.addFullFaculty(sectionPromise);
-    // }
-
-    /**
-     * Gets all sections for a specific department in this term
-     * @param dept The code of the department in this term
-     * @param withFaculty Whether or not to add hydrated faculty
-     */
-    async sectionsForDepartment(dept: string, withFaculty = false) {
-        await this.addCookies();
-        const sectionPromise = this.fetchAndCombinePaginatedResponses<CompassSection>(
-            `/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_subject=${dept}&txt_term=${this.termCode}`,
-            urlString =>
-                this.client
-                    .get(urlString, {withCredentials: true})
-                    .then(response => response.data.data)
-        );
-        if (!withFaculty) {
-            return sectionPromise;
-        }
-        return this.addFullFaculty(sectionPromise);
-    }
-
-    /**
-     * Gets hydrated faculty members, given their "dehydrated" versions.
-     * @param facultyMembers
-     */
-    async faculty(
-        ...facultyMembers: CompassSectionFaculty[]
-    ): Promise<CompassFaculty[]> {
-        const fullFaculty: CompassFaculty[] = [];
-        for (const facultyMember of facultyMembers) {
-            if (!this.professorCache.has(facultyMember.displayName)) {
-                const response = await this.client.get(
-                    `/StudentRegistrationSsb/ssb/contactCard/retrieveData?bannerId=${facultyMember.bannerId}&termCode=${this.termCode}`,
-                    {
-                        withCredentials: true,
-                    }
-                );
-                const fullMember = response.data.data.personData as CompassFaculty;
-                if (fullMember.cvExists) {
-                    fullMember.cvUrl = BASE_URL + fullMember.cvUrl;
-                }
-                this.professorCache.set(facultyMember.displayName, fullMember);
-            }
-            fullFaculty.push(this.professorCache.get(facultyMember.displayName)!);
-        }
-        return fullFaculty;
-    }
-
-    static async allTerms(): Promise<Array<{ code: string; description: string }>> {
-        const response = await Axios.get(
-            BASE_URL +
-            `/StudentRegistrationSsb/ssb/courseSearch/getTerms?dataType=json&offset=1&max=${Term.MAX_PAGE_SIZE}`
-        );
-        return response.data;
-    }
+  static async allTerms(): Promise<
+    Array<{ code: string; description: string }>
+  > {
+    const response = await Axios.get(
+      BASE_URL +
+        `/StudentRegistrationSsb/ssb/courseSearch/getTerms?dataType=json&offset=1&max=${Term.MAX_PAGE_SIZE}`
+    );
+    return response.data;
+  }
 }
